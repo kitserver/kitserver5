28981a9bd08112a08de53d99b19f3ba608962ad1
Anton Jouline <anton.jouline@cbsinteractive.com>
2017-12-15 18:00:51 -0800

    doc updates; version bump for 5.5.3 release

--- src/setup.h
+++ src/setup.h
@@ -6,7 +6,7 @@
 #else
 #define SETUP_WINDOW_TITLE "KitServer 5 Setup (debug build)"
 #endif
-#define CREDITS "About: v5.5.1 (12/2017) by Juce and Robbie."
+#define CREDITS "About: v5.5.3 (12/2017) by Juce and Robbie."
 
 #define LOG(f,x) if (f != NULL) fprintf x
 
